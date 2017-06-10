1594505eed4918adc8f56e4d440b383fce74f163
vgaidarji <veaceslav.gaidarji@gmail.com>
2017-06-10 20:59:00 +0300

    Print artifacts list in JSON format

---
+++ build-install-run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./build-install.sh
+./gradlew :sample:android-app:dependenciesOverview
--- dependencies-overview/src/main/kotlin/com/vgaidarji/dependencies/overview/DependenciesOverviewTask.kt
+++ dependencies-overview/src/main/kotlin/com/vgaidarji/dependencies/overview/DependenciesOverviewTask.kt
@@ -1,6 +1,8 @@
 package com.vgaidarji.dependencies.overview
 
+import groovy.json.JsonOutput
 import org.gradle.api.DefaultTask
+import org.gradle.api.artifacts.ResolvedModuleVersion
 import org.gradle.api.tasks.TaskAction
 
 open class DependenciesOverviewTask : DefaultTask() {
@@ -12,6 +14,26 @@ open class DependenciesOverviewTask : DefaultTask() {
 
     @TaskAction
     fun generate() {
-        println("---DependenciesOverviewTask---")
+        println(asJson(getArtifactsForConfiguration()))
+    }
+
+    fun getArtifactsForConfiguration(configuration: String = "compile") :
+            MutableList<ResolvedModuleVersion> {
+        val artifacts = mutableListOf<ResolvedModuleVersion>()
+        val config = project.configurations.getByName(configuration).resolvedConfiguration
+        config.resolvedArtifacts.forEach {
+            artifacts.add(it.moduleVersion)
+        }
+        return artifacts
+    }
+
+    private fun asJson(artifacts: MutableList<ResolvedModuleVersion>): String {
+        var json = "{\n\"dependencies\": ["
+        artifacts.forEachIndexed { index, resolvedModuleVersion ->
+            // println "group: ${id.group}, name: ${id.name}, version: ${id.version}"
+            if (index > 0) json = json.plus(",")
+            json = json.plus(JsonOutput.toJson(resolvedModuleVersion.id))
+        }
+        return JsonOutput.prettyPrint(json.plus("]\n}"))
     }
 }
