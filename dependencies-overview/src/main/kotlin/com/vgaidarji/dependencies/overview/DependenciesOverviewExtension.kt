b39f1f6f09133d51ce68ea5921e73df14e4c2c3b
vgaidarji <veaceslav.gaidarji@gmail.com>
2017-07-06 10:59:30 +0300

    Enhance documentation of extension class

--- dependencies-overview/src/main/kotlin/com/vgaidarji/dependencies/overview/DependenciesOverviewExtension.kt
+++ dependencies-overview/src/main/kotlin/com/vgaidarji/dependencies/overview/DependenciesOverviewExtension.kt
@@ -1,10 +1,19 @@
 package com.vgaidarji.dependencies.overview
 
 open class DependenciesOverviewExtension {
+    /**
+     * Specifies output format.
+     */
     var output = OutputFormat()
 
     open class OutputFormat {
+        /**
+         * Dependencies list will be printed out in markdown format.
+         */
         var markdown = true
+        /**
+         * Dependencies list will be printed out in json format.
+         */
         var json = false
     }
 }
