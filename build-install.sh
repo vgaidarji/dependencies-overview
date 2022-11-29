9d2f2f5a8cb05f2360021f344aeaf8a07b493f53
vgaidarji <veaceslav.gaidarji@gmail.com>
2022-11-29 15:05:02 +0200

    Deploy different plugin version locally
    useLocalVersion parameter is needed to deploy locally library version that will be further referenced
    in integration project. This is to avoid accidental usage of remote library version
    published to maven central and to always consume latest version and make sure all tests are passing

--- build-install.sh
+++ build-install.sh
@@ -5,4 +5,7 @@ set -e
 rm -rf repo
 ./gradlew clean
 ./gradlew :dependencies-overview:build
-./gradlew publishToMavenLocal -Dmaven.repo.local="$(pwd)"/repo
+# useLocalVersion is needed to deploy locally library version that will be further referenced
+# in integration project. This is to avoid accidental usage of remote library version
+# published to maven central and to always consume latest version and make sure all tests are passing
+./gradlew publishToMavenLocal -DuseLocalVersion=true -Dmaven.repo.local="$(pwd)"/repo
